e2d74e74acfe1c19efe2b8973a87ed56d8016cd6
Sagar Bhoi <sagar.bhoi@veracity-india.com>
2025-05-08 15:16:20 +0530

    Cloud Config Server & Client

---
+++ 08_Cloud_Config_Client/src/main/java/com/sagarsoft/controller/MessageRestController.java
@@ -0,0 +1,18 @@
+package com.sagarsoft.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageRestController {
+
+	@Value("${msg}")
+	private String msg;
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return msg;
+	}
+}
+
