3c91d16bee58a5dafb249bb2e8494863de75815b
Sagar Bhoi <sagar.bhoi@veracity-india.com>
2025-05-08 14:14:45 +0530

    Load balancing

--- 04_Welcome_Service/src/main/java/com/sagarsoft/controller/WelcomeController.java
+++ 04_Welcome_Service/src/main/java/com/sagarsoft/controller/WelcomeController.java
@@ -1,14 +1,22 @@
 package com.sagarsoft.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/welcome")
 	public String getWelcomeMessage() {
-		String msg = "Welcome to SagarSoft";
+		
+		String port = env.getProperty("server.port");
+		
+		String msg = "Welcome to SagarSoft(" + port + ")" ;
 		return msg;
 	}
 }
